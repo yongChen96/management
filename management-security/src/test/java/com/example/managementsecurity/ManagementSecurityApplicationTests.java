a560af6ceb3d645b31a4c40245bf07f8a926b8ef
yongchen <cy19950906>
2020-08-28 17:14:18 +0800

    init

---
+++ management-security/src/test/java/com/example/managementsecurity/ManagementSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.managementsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagementSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
